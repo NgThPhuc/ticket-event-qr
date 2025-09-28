294af017b9e93733392a5e0725fe359285dc6b9a
NgThPhucs <nguyenphuch5p@gmail.com>
2025-09-28 11:50:47 +0700

    create n edit event

--- convex/storage.ts
+++ convex/storage.ts
@@ -1,6 +1,13 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
+export const getUrl = query({
+  args: { storageId: v.id("_storage") },
+  handler: async (ctx, { storageId }) => {
+    return await ctx.storage.getUrl(storageId);
+  },
+});
+
 export const generateUploadUrl = mutation(async (ctx) => {
   return await ctx.storage.generateUploadUrl();
 });
@@ -14,13 +21,6 @@ export const updateEventImage = mutation({
     await ctx.db.patch(eventId, {
       imageStorageId: storageId ?? undefined,
     });
-  },
-});
-
-export const getUrl = query({
-  args: { storageId: v.id("_storage") },
-  handler: async (ctx, { storageId }) => {
-    return await ctx.storage.getUrl(storageId);
   },
 });
 
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "kindly-tiger-902.convex.cloud", protocol: "https" },
+      // { hostname: "wary-anaconda-29.convex.cloud", protocol: "https" },
+    ],
+  },
 };
 
 export default nextConfig;
